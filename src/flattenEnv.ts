6e784a9a67bed26d29b0e76655ec0eab5e7a29ff
Ray Hao <2468048176@qq.com>
2023-11-13 17:07:34 +0800

    fix: 🐛 修复 flattenEnv 扁平化非正常属性名报错问题

--- src/flattenEnv.ts
+++ src/flattenEnv.ts
@@ -1,11 +1,14 @@
 import type { Recordable } from '@rhao/types-base'
 import { isObjectLike, isString } from 'lodash-es'
 
+const ignoreAttrRE = /(^\d|[^\w])/g
+
 /**
  * 扁平化环境变量，用于 `define` 时替换复杂变量
  *
  * - 数组取值：仅支持 `env[index]` 格式
  * - 非数组取值：仅支持 `env.aaa.bbb` 格式
+ * - 非正常属性名：以数字开头或包含非字母、数字、下划线的属性名由于无法被 `define`，将被忽略
  *
  * ***注意：扁平化后请不要再 `define` 同 `name` 的变量，否则编译时可能回导致失败！***
  *
@@ -15,7 +18,7 @@ import { isObjectLike, isString } from 'lodash-es'
  *
  * @example
  * ```ts
- * flattenEnv({ a: { b: 1, c: [1, 2, 3] }, d: '4' }, '__ENV__')
+ * flattenEnv({ a: { b: 1, c: [1, 2, 3] }, d: '4', 'e-5': 6 }, '__ENV__')
  * // 扁平化后
  * {
  *   '__ENV__.a.b': 1,
@@ -24,7 +27,8 @@ import { isObjectLike, isString } from 'lodash-es'
  *   '__ENV__.a.c[1]': 2,
  *   '__ENV__.a.c[2]': 3,
  *   '__ENV__.a': { b: 1, c: [1, 2, 3] },
- *   '__ENV__.d': '"4"'
+ *   '__ENV__.d': '"4"',
+ *   // '__ENV__.e-5': 6 // ❌ 由于属性名非正常格式，该属性将被忽略
  * }
  * ```
  */
@@ -35,6 +39,9 @@ export function flattenEnv(name: string, env: Recordable) {
   const deep = (env: Recordable, paths: string[]) => {
     const isArr = Array.isArray(env)
     Object.entries(env).forEach(([key, value]) => {
+      // 忽略无法 define 的属性
+      if (ignoreAttrRE.test(key)) return
+
       const _paths = paths.concat(isArr ? `[${key}]` : key)
       // 如果是 object 类型则递归遍历
       if (isObjectLike(value)) deep(value, _paths)
